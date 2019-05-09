67478cf5178cb84fc3408e2d7d479eb4ca4c3a3a
Will Chisholm <william.f.chisholm.iii.20@dartmouth.edu>
2019-05-09 15:06:49 -0400

    done with ws

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 // import your npm packages
+import ApolloClient from 'apollo-boost';
+import { GetRepos, AddStar, RemoveStar } from './operations';
 
 // keys for actiontypes
 export const ActionTypes = {
@@ -8,24 +10,66 @@ export const ActionTypes = {
 };
 
 const GITHUB_API = 'https://api.github.com/graphql';
-const API_KEY = 'YOUR_KEY';
+const API_KEY = '08b23eb51325d7ae3002478f7c0d91b51f2c4f3b';
+
+const client = new ApolloClient({
+  uri: GITHUB_API,
+  headers: { authorization: `bearer ${API_KEY}` },
+});
 
 // initialize ApolloClient here
 
 export function fetchRepos(query) {
   return (dispatch) => {
-    // fetchRepos query
+    client.query({
+      query: GetRepos,
+      variables: {
+        queryString: query,
+      },
+      fetchPolicy: 'no-cache',
+    })
+      .then((response) => {
+        const repos = response.data.search.edges[0].node.repositories.edges.map(repo => repo.node);
+        dispatch({ type: ActionTypes.FETCH_REPOS, payload: repos });
+      })
+      .catch((error) => {
+        dispatch({ type: ActionTypes.ERROR_SET, error });
+      });
   };
 }
 
 export function addStar(repoID, searchTerm) {
   return (dispatch) => {
-    // addStar mutation
+    client.mutate({
+      mutation: AddStar,
+      variables: {
+        id: repoID,
+      },
+      fetchPolicy: 'no-cache',
+    })
+      .then((res) => {
+        dispatch(fetchRepos(searchTerm));
+      })
+      .catch((error) => {
+        dispatch({ type: ActionTypes.ERROR_SET, error });
+      });
   };
 }
 
 export function removeStar(repoID, searchTerm) {
   return (dispatch) => {
-    // removeStar mutation
+    client.mutate({
+      mutation: RemoveStar,
+      variables: {
+        id: repoID,
+      },
+      fetchPolicy: 'no-cache',
+    })
+      .then((response) => {
+        dispatch(fetchRepos(searchTerm));
+      })
+      .catch((error) => {
+        dispatch({ type: ActionTypes.ERROR_SET, error });
+      });
   };
 }
